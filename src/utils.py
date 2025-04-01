09cc5661f57c8b355a8eaf24dd84e51f4b82b2ec
pstreb <pablo.streb@gmail.com>
2025-04-01 12:59:00 -0300

    New version

--- src/utils.py
+++
@@ -1,5 +0,0 @@
-def utility_function():
-    """
-    This is a utility function.
-    """
-    return "This is a utility!"
--- tests/test_utils.py
+++
@@ -1,4 +0,0 @@
-def test_utility_function():
-    from src.utils import utility_function
-
-    assert utility_function() == "This is a utility!"
